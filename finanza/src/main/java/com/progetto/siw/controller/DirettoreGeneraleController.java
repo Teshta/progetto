812d93b1c0d875effb127ff77c9c1d1b832dd1d4
damiano <damiano@puffyrabbit>
2018-06-05 15:34:35 +0200

    cancellazione

--- finanza/src/main/java/com/progetto/siw/controller/DirettoreGeneraleController.java
+++ finanza/src/main/java/com/progetto/siw/controller/DirettoreGeneraleController.java
@@ -7,7 +7,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DirettoreGeneraleController {
 	
-//	@RequestMapping("/direttore", method = RequestMethod.GET)
-//	public String getDirettore(Model model) {
-//	}
+
 }
